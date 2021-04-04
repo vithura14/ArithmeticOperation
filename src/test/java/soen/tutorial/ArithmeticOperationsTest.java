a58c77526648ebb2c350da856fc13ad36d97393e
vithura14 <60198384+vithura14@users.noreply.github.com>
2021-04-03 21:26:02 -0400

    Update ArithmeticOperationsTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -3,18 +3,20 @@ package soen.tutorial;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class ArithmeticOperationsTest {
 
 
-	@Test
-	public void testAdd()
-	{
+	//@Test
+//	public void testAdd()
+	//{
 
 		ArithmeticOperations operations = new ArithmeticOperations();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		assertEquals(expected, actual);
+		//assertEquals(expected, actual);
+		fail("must fail");
 
 	}
    // @Test
@@ -28,4 +30,4 @@ public class ArithmeticOperationsTest {
 
   //  }
 
-}
+//}
